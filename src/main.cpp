0d0d1c32eba84abe896970cd03c8c279792c04cc
oylonq <oylonq@gmail.com>
2026-06-02 15:05:31 +0800

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,19 @@ int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // TODO: Uncomment the code below to pass the first stage
-  std::cout << "$ ";
+  while (true) {
+    // Read: Display a prompt and wait for user input
+    std::cout << "$ ";
 
-  std::string input;
-  std::cin >> input;
+    std::string input;
+    std::getline(std::cin, input);
 
-  std::cout << input << ": command not found" << '\n';
+    // Eval: Parse and execute the command
+
+    // Print: Display the output or error message
+    std::cout << input << ": command not found" << '\n';
+
+  } // Loop: Return to step 1 and wait for the next command
+
   return 0;
 }
