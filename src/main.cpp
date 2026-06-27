8ec74d4cd8476b4044dba5f3f1732fbe3493fdec
oylonq <oylonq@gmail.com>
2026-06-27 16:02:04 +0800

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -5,18 +5,27 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
-#include <unordered_set>
+#include <unordered_map>
 #include <vector>
 
 namespace Shell {
 std::vector<std::string> argv;
 };
-
-std::unordered_set<std::string> builtin{"echo", "exit", "type", "pwd", "cd"};
+enum class CMDS { EXIT, ECHO, TYPE, PWD, CD };
+std::unordered_map<std::string, CMDS> builtin{{"exit", CMDS::EXIT},
+                                              {"echo", CMDS::ECHO},
+                                              {"type", CMDS::TYPE},
+                                              {"cd", CMDS::CD}};
 
 std::string trimStr(std::string &input);
 std::vector<std::string> splitStr(std::string &s, char symbol);
 bool getPath(std::string &command, std::string &cmd_path);
+
+void exitBuiltin();
+void echoBuiltin();
+void typeBuiltin();
+void pwdBuiltin();
+void cdBuiltin();
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -35,69 +44,28 @@ int main() {
     std::string trimed_input = trimStr(input);
     Shell::argv = splitStr(trimed_input, ' ');
 
-    if (Shell::argv[0] == "exit") {
+    switch (builtin[Shell::argv[0]]) {
+    case CMDS::EXIT:
+      exitBuiltin();
+      break;
 
-      return 0;
+    case CMDS::ECHO:
+      echoBuiltin();
+      break;
 
-    } else if (Shell::argv[0] == "echo") {
-      for (int i = 1; i < Shell::argv.size(); ++i) {
-        std::cout << Shell::argv[i] << ' ';
-      }
-      std::cout << '\n';
+    case CMDS::TYPE:
+      typeBuiltin();
+      break;
 
-    } else if (Shell::argv[0] == "type") {
+    case CMDS::PWD:
+      pwdBuiltin();
+      break;
 
-      if (Shell::argv.size() > 0) {
-        if (builtin.find(Shell::argv[1]) != builtin.end()) {
-          std::cout << Shell::argv[1] << " is a shell builtin" << '\n';
+    case CMDS::CD:
+      cdBuiltin();
+      break;
 
-        } else { // cmd is not a builtin
-          // Get PATH
-          std::string PATH{getenv("PATH")};
-          std::vector<std::string> PATHs = splitStr(PATH, ':');
-          std::string full_path;
-          bool is_exec = false;
-
-          for (std::string &path : PATHs) {
-            full_path = path + "/" + Shell::argv[1];
-            if (std::filesystem::exists(full_path)) {
-              std::filesystem::perms p =
-                  std::filesystem::status(full_path).permissions();
-
-              is_exec = std::filesystem::perms::none !=
-                        ((std::filesystem::perms::owner_exec |
-                          std::filesystem ::perms ::group_exec |
-                          std::filesystem::perms::others_exec) &
-                         p);
-
-              if (is_exec) {
-                break;
-              }
-            }
-          }
-
-          if (is_exec) {
-            std::cout << Shell::argv[1] << " is " << full_path << '\n';
-          } else {
-            std::cout << Shell::argv[1] << ": not found" << '\n';
-          }
-        }
-      }
-
-    } else if (Shell::argv[0] == "pwd") {
-      std::filesystem::path cwd = std::filesystem::current_path();
-      std::cout << std::string(cwd) << '\n';
-
-    } else if (Shell::argv[0] == "cd") {
-      std::filesystem::path target_dir(Shell::argv[1]);
-      if (std::filesystem::is_directory(target_dir)) {
-        std::filesystem::current_path(target_dir);
-      } else {
-        std::cout << "cd: " << Shell::argv[1] << ": No such file or directory"
-                  << '\n';
-      }
-
-    } else {
+    default: {
       // Determine if the give command is an executable
       std::string cmd_path;
       if (getPath(Shell::argv[0], cmd_path)) {
@@ -125,10 +93,12 @@ int main() {
 
         std::cout << Shell::argv[0] << ": command not found" << '\n';
       }
+    } break;
     }
 
     int status;
     waitpid(pid, &status, 0);
+
   } // Loop: Return to step 1 and wait for the next command
 
   return 0;
@@ -190,4 +160,67 @@ std::vector<std::string> splitStr(std::string &s, char symbol) {
   res.push_back(s.substr(start));
 
   return res;
+}
+
+void exitBuiltin() { exit(0); }
+
+void echoBuiltin() {
+  for (int i = 1; i < Shell::argv.size(); ++i) {
+    std::cout << Shell::argv[i] << ' ';
+  }
+  std::cout << '\n';
+}
+
+void typeBuiltin() {
+  if (Shell::argv.size() > 0) {
+    if (builtin.find(Shell::argv[1]) != builtin.end()) {
+      std::cout << Shell::argv[1] << " is a shell builtin" << '\n';
+
+    } else { // cmd is not a builtin
+      // Get PATH
+      std::string PATH{getenv("PATH")};
+      std::vector<std::string> PATHs = splitStr(PATH, ':');
+      std::string full_path;
+      bool is_exec = false;
+
+      for (std::string &path : PATHs) {
+        full_path = path + "/" + Shell::argv[1];
+        if (std::filesystem::exists(full_path)) {
+          std::filesystem::perms p =
+              std::filesystem::status(full_path).permissions();
+
+          is_exec = std::filesystem::perms::none !=
+                    ((std::filesystem::perms::owner_exec |
+                      std::filesystem ::perms ::group_exec |
+                      std::filesystem::perms::others_exec) &
+                     p);
+
+          if (is_exec) {
+            break;
+          }
+        }
+      }
+
+      if (is_exec) {
+        std::cout << Shell::argv[1] << " is " << full_path << '\n';
+      } else {
+        std::cout << Shell::argv[1] << ": not found" << '\n';
+      }
+    }
+  }
+}
+
+void pwdBuiltin() {
+  std::filesystem::path cwd = std::filesystem::current_path();
+  std::cout << std::string(cwd) << '\n';
+}
+
+void cdBuiltin() {
+  std::filesystem::path target_dir(Shell::argv[1]);
+  if (std::filesystem::is_directory(target_dir)) {
+    std::filesystem::current_path(target_dir);
+  } else {
+    std::cout << "cd: " << Shell::argv[1] << ": No such file or directory"
+              << '\n';
+  }
 }
